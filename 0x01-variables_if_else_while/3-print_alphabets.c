08890db7ec5b4338fd1ee39775afc9a42f613bd2
Trevor-george1 <Trevormahinda@gmail.com>
2023-03-17 11:36:48 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 	char c;
-	char caps;
+	
 
 	for (c = 'a'; c <= 'z'; c++)
-	for (caps = 'A'; caps <= 'Z'; caps++)
 	putchar(c);
-	putchar(toupper(caps));
+	for (c = 'A'; c <= 'Z'; c++)
+	
+	putchar(c);
 	putchar('\n');
 	return (0);
 
