937dc3190f6e6db3b2c213bbe2d87f601a456c04
Trevor-george1 <Trevormahinda@gmail.com>
2023-03-17 12:41:45 -0700

    Invention

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always
+*/
+int main(void)
+{
+	int d, i;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	for (i = d + 1; i <= '9'; i++)
+	{
+	if (i != d)
+	{
+	putchar(d);
+	putchar(i);
+	if (d == '8' && i == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+
+
+}
