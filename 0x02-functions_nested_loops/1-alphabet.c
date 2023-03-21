d7cf5a12a18a69b37bcca895d2769edbb7111068
Trevor-george1 <Trevormahinda@gmail.com>
2023-03-21 04:32:22 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
 #include "main.h"
 /**
  * main - Entry point
- * Return: Always success
+ * Return: void
+ * print_alphabet - make the alphabet
  *
- */
-
+*/
 void print_alphabet(void)
 {
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 	_putchar(i);
 	}
 	_putchar('\n');
-
-
-
 }
