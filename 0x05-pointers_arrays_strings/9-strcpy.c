c47715295d24f88d07e9cf150f10fe17bd27f7db
Trevor-george1 <Trevormahinda@gmail.com>
2023-03-27 06:22:50 -0700

    Last tasks

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_strcpy - copies the string pointed to by src 
+ * *_strcpy - copies the string pointed to by src
  * @src: Source value
  * @Dest: Destination value
  * return: Always success
@@ -9,6 +9,7 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
