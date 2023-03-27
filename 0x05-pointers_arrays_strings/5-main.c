36924304080a4e6047f4e163309469f05bcd3245
Trevor-george1 <Trevormahinda@gmail.com>
2023-03-27 03:29:29 -0700

    Last commit

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char s[10] = "My School";
+	char s[11] = "Hoberlton!";
 
 	printf("%s\n", s);
 	rev_string(s);
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,22 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
 /**
  * rev_string - reverses a string
  * @s: Var to be checked
+ * Return: Nothing
  */
 
 void rev_string(char *s)
 {
-	int l, i;
-	char *begin_ptr, *end_ptr, ch;
+	int len = 0, index = 0;
+	char tmp;
 
-	l = strlen(s);
-	begin_ptr = s;
-	end_ptr = s + l - 1;
-	for (i = 0; i < (l - 1) / 2; i++)
+	while (s[index++])
+	len++;
+
+	for (index = len -1; index >= len /2; index--)
 	{
-	ch = *end_ptr;
-	*end_ptr = *begin_ptr;
-	*begin_ptr = ch;
-	begin_ptr++;
-	end_ptr--;
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
 }
