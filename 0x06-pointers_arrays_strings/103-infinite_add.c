6f6ea980866384f4fea7ea12dd831b3733bf67b4
Trevor-george1 <Trevormahinda@gmail.com>
2023-03-30 08:47:50 -0700

    Task 10

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,35 +9,72 @@
  * @r: store the result
  * @size_r: size of variable r
  * Return: r
+ *
+ *
+*
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+	i++;
+	}
+	i--;
+	for (j = 0; j < i; j++, i--)
+	{
+	temp = *(n + j);
+	*(n + j) = *(n + i);
+	*(n + i) = temp;
+	}
+}
 
 char *infinite_add(char *n, char *m, char *r, int size_r)
 {
-	int len_n = strlen(n);
-	int len_m = strlen(m);
-	int i, j, digit_n, digit_m, digit_sum, k;
-	int len_sum = (len_n > len_m ? len_n : len_m) + 1;
-	int carry = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	if (len_sum > size_r)
+	while (*(n + i) != '\0')
+	i++;
+	while (*(m + j) != '\0')
+	j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+	return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-	printf("Error\n");
-	return NULL;
+	if (i < 0)
+	val1 = 0;
+	else
+	val1 = *(n + i) - '0';
+	if (j < 0)
+	val2 = 0;
+	else
+	val2 = *(m + j) - '0';
+	temp_tot = val1 + val2 + overflow;
+	if (temp_tot >= 10)
+	overflow = 1;
+	else
+	overflow = 0;
+	if (digits >= (size_r - 1))
+	return (0);
+	*(r + digits) = (temp_tot % 10) + '0';
+	digits++;
+	j--;
+	i--;
 	}
-	for (i = len_n - 1, j = len_m - 1, k = len_sum - 1; i >= 0 || j >= 0 || carry > 0; i--, j--, k--)
-	{
-	digit_n = (i >= 0 ? n[i] - '0' : 0);
-	digit_m = (j >= 0 ? m[j] - '0' : 0);
-	digit_sum = digit_n + digit_m + carry;
-	carry = digit_sum / 10;
-	digit_sum %= 10;
-	r[k] = digit_sum + '0';
-	}
-	r[len_sum] = '\0';
-	while (r[0] == '0' && len_sum > 1)
-	{
-	r++;
-	len_sum--;
-	}
+	if (digits == size_r)
+	return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
+
