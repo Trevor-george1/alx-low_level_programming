b9a434eae706e30a5c8e34dbdda6de02e8c0814b
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-03 11:00:03 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - function tgat sets the value
+ *  of a pointer to a char
+ *  @s: pointer to *to
+ *  @to: variable
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
