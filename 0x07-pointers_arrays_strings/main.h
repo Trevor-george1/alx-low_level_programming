7daf1c866d39e89ab191714d06833f2c7071b982
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-03 05:24:36 -0700

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,11 @@
+#ifndef _main_H_
+#define _main_H_
+
+
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
+
+#endif
