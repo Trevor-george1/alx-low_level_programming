fa12b7236e049a2af00fd2cc6f8c18b6d38c9ca9
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-14 13:32:35 +0000

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: variable stored in the memory
+ * Return: pointer to memory address
+ */
+
+void *malloc_checked(unsigned int b)
+{	
+	
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+	fprintf(stderr, "Error: Malloc failed: %s\n", strerror(errno));
+	exit(98);
+	}
+	return (p);
+}
