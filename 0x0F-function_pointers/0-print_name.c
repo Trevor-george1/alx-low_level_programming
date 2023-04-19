29b96719204d37790af4a7184b021543a40e4f8a
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-19 09:35:33 +0000

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * f - prints a name
+ *
+ * @f: is a pointer to function print_name
+ *
+ * Return: Nothing
+ */
+void *f(char *n)
+{
+	int len, i;
+	len = strlen(n);
+
+	for (i = 0; i < len; i++)
+	putchar(n[i]);
+	return (0);
+}
+
+/**
+ * print_name - prints a name
+ * @f: pointer to function print_name
+ * @name: name to be printed
+ * Return: Nothing
+ */
+
+void print_name(char *name, void(*f)(char *n))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+void print_name(char *name, void (*f)(char *));
+
+int _putchar(char c);
+#endif
