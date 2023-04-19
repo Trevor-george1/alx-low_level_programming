e99073d9f06a436fafc094a1279fd9f2266b408e
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-19 16:42:12 +0000

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -24,6 +24,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (cmp && array)
+	{
 	if (size <= 0)
 	return (-1);
 	for (i = 0; i < size; i++)
@@ -31,6 +33,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (cmp(array[i]))
 	{
 	return (i);
+	}
 	}
 	}
 	return (-1);
