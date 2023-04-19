fa6f6cb7d046a23ed7622f6bcf8778155c55f8fe
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-19 16:55:23 +0000

    Last last

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,28 +2,6 @@
 #include <stdlib.h>
 
 /**
- * print_codes - prints opcodes of its own main
- * @no_of_bytes: number of bytes
- * Return: Nothing
- */
-
-void print_codes(int no_of_bytes)
-{
-	int i;
-	unsigned char *p = (unsigned char *)print_codes;
-
-	for (i = 0; i < no_of_bytes; i++)
-	{
-	if (i == no_of_bytes - 1)
-	{
-	printf("%02x\n", p[i]);
-	break;
-	}
-	printf("%02x ", p[i]);
-	}
-}
-
-/**
  * main - entry point of program
  * @argc: number of arguments
  * @argv: array of arguments
@@ -33,18 +11,29 @@ void print_codes(int no_of_bytes)
 int main(int argc, char *argv[])
 {
 	int num_of_bytes = 0;
+	char *p;
+	int i;
 
 	if (argc != 2)
 	{
 	printf("Error\n");
-	return (1);
+	exit(1);
 	}
 	num_of_bytes = atoi(argv[1]);
 	if (num_of_bytes < 0)
 	{
 	printf("Error");
-	return (2);
+	exit(2);
 	}
-	print_codes(num_of_bytes);
+	p = (char *)main;
+	for (i = 0; i < num_of_bytes; i++)
+	{
+	if (i == num_of_bytes - 1)
+	{
+	printf("%02hhx\n", p[i]);
+	break;
+	}
+	printf("%02hhx ", p[i]);
+	}
 	return (0);
 }
