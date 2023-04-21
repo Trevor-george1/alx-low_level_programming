030b2d827d4f03ea39c20fcf7e86f276da62d701
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-21 19:45:10 +0000

    Last last

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -17,14 +17,13 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 	printf("Error\n");
-	exit(1);
+	return (1);
 	}
 	num_of_bytes = atoi(argv[1]);
 	if (num_of_bytes < 0)
 	{
-	printf("Error");
-	return (1);
-	exit(2);
+	printf("Error\n");
+	return (2);
 	}
 	p = (char *)main;
 	for (i = 0; i < num_of_bytes; i++)
