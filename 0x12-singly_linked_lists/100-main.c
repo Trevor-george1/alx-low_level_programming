819e6930625ad8b047e670e6b5c7fd0717af10dc
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-27 15:39:22 +0000

    Hare

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -5,11 +5,8 @@
  * Return: Always 0.
  */
 
-void before_main(void);
-
 int main(void)
 {
-	before_main();
 	printf("(A tortoise, having pretty good sense of a hare's nature,");
 	printf("challenges one to a race.)\n");
 	return (0);
