8a0f7628db092eabd056f9a91009c4679fea380c
Trevor-george1 <Trevormahinda@gmail.com>
2023-04-27 15:32:11 +0000

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -10,7 +10,7 @@ void before_main(void);
 int main(void)
 {
 	before_main();
-	printf("(A tortoise, having pretty good sense of a hare's nature,")
+	printf("(A tortoise, having pretty good sense of a hare's nature,");
 	printf("challenges one to a race.)\n");
 	return (0);
 }
