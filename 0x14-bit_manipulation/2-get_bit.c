ff7ca72021e8e474cb15c2150f1eddc073804f2b
Trevor-george1 <Trevormahinda@gmail.com>
2023-05-04 17:19:15 +0000

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int m;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	unsigned long int m = 1UL << index;
-
+	m = 1UL << index;
 	return ((n & m) ? 1 : 0);
 }
