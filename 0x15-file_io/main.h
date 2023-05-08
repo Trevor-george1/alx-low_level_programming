e35f5d9fb7f5a848d95c81801766d165976a609f
Trevor-george1 <Trevormahinda@gmail.com>
2023-05-08 16:39:08 +0000

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,8 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+void err_exit(int file_from, int file_to, char *argv[]);
+
+
+
 #endif
