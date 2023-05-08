d787319ba1813ae2d1100df89cf7bbda7260278b
Trevor-george1 <Trevormahinda@gmail.com>
2023-05-08 15:01:22 +0000

    Under the snow

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+
+/**
+ * read_text - reads a text file and prints it to standard output
+ * @filename: name of the file
+ * @letters: number of letters it should print
+ * Return: number of letters it could read and print
+ */
+
+size_t read_text(const char *filename, size_t letters)
+{
+	int f;
+	char *buff;
+	ssize_t no_bytes, write_bytes;
+
+	if (filename == NULL)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+	{
+		return (0);
+	}
+	buff = malloc((letters + 1) * sizeof(char));
+	if (buff == NULL)
+	{
+		close(f);
+		return (0);
+	}
+	no_bytes = read(f, buff, letters);
+	if (no_bytes == -1)
+	{
+		close(f);
+		free(buff);
+		return (0);
+	}
+	write_bytes = write(STDOUT_FILENO, buff, no_bytes);
+	if (write_bytes == -1 || write_bytes != no_bytes)
+	{
+		close(f);
+		free(buff);
+		return (0);
+	}
+
+	close(f);
+	free(buff);
+	return (write_bytes);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * create_file - creats a file
+ * @filename: name of file to create
+ * @text_content: string to write to the file
+ * Return: 1 on succes -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	mode_t mode;
+	int f;
+	ssize_t b;
+
+	if (filename == NULL)
+		return (-1);
+	mode = S_IRUSR | S_IWUSR;
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (f == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+	b = write(f, text_content, strlen(text_content));
+	if (b == -1)
+	{
+		close(f);
+		return (-1);
+	}
+	}
+	close(f);
+	return (-1);
+}
