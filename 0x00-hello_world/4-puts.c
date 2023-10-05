fdfb685b9eff11faaf24bebe60830a5056ad624c
Trevor <Trevormahinda@gmail.com>
2023-10-05 13:06:25 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *message = "\"Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
+}
