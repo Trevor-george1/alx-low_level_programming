698e4e5395ec1c06d2b6b5a535eb7b508ba6074a
Trevor <Trevormahinda@gmail.com>
2023-10-12 12:13:58 +0000

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - like the func name says
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
