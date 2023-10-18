487cbac73389995220783a09fe98e112ca8a0fe0
Trevor <Trevormahinda@gmail.com>
2023-10-18 21:38:19 +0000

    CODS

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - changes owercase letters to upper
+ * @s: pointer to string
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 122 && s[i] >= 97)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
