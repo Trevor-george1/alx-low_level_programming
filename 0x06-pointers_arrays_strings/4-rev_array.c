80360d0f5bc2e10f80141df8d559730b84564b00
Trevor <Trevormahinda@gmail.com>
2023-10-18 21:28:13 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverse an array of int
+ * @a: pointer to array of int
+ * @n: size
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp, s;
+	
+	i = 0;
+	s = n - 1;
+	while (i < s)
+	{
+		temp = a[i];
+		a[i] = a[s];
+		a[s] = temp;
+		i++;
+		s--;
+	}
+}
