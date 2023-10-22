ac488c4a84d52609abfa659105db4e1f99f6153f
Trevor <Trevormahinda@gmai.com>
2023-10-22 14:26:51 +0300

    update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * string_toupper - changes owercase letters to upper
+ * string_toupper - changes lowercase letters to upper
  * @s: pointer to string
  * Return: char
  */
 char *string_toupper(char *s)
 {
 	int i = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
+
 	{
 		if (s[i] <= 122 && s[i] >= 97)
 		{
