2d09f41c3f181a97b1d93a2318220a1b4a6de84c
Trevor <Trevormahinda@gmai.com>
2023-10-23 20:19:06 +0300

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
 	unsigned int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
 	for (j = 0; j < n; j++)
 	{
-		dest[i] = src[j];
+		dest[j] = src[j];
 	}
 	return (dest);
 }
