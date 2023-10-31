60d668061e60fdf404aaadbf97a0c7f35b52def4
Trevor <Trevormahinda@gmai.com>
2023-10-31 20:48:44 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-	if (s1 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-		
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	a = malloc((len1 * sizeof(char)) + (len2 + 1 * sizeof(char)));
