876a126e69fce3f40a75fb5478c9c0e4886b40fd
Trevor <Trevormahinda@gmai.com>
2023-11-06 20:10:43 +0300

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,21 +53,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new = malloc(sizeof(dog_t));
+	new = (dog_t *)malloc(sizeof(dog_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	new->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (new->name == NULL)
+	if ((*new).name == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 	new->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (new->owner == NULL)
+	if ((*new).owner == NULL)
 	{
-		free(new->owner);
+		free(new->name);
 		free(new);
 		return (NULL);
 	}
