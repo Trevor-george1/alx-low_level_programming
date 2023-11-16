237683b1134d1a4e8e3027f755c78759fd51d87f
Trevor <Trevormahinda@gmai.com>
2023-11-17 00:51:25 +0300

    update: main fnction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,11 +8,7 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
-	while (h->next != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
@@ -22,9 +18,8 @@ size_t print_list(const list_t *h)
 		{
 		printf("[%d] %s\n", h->len, h->str);
 		}
-		h = h->next;
 		i++;
+		h = h->next;
 	}
-	printf("[%d] %s\n", h->len, h->str);
 	return (i);
 }
