eafb9852c80024f1a57a4c07149bfce58868c206
Trevor <Trevormahinda@gmai.com>
2023-11-17 20:58:10 +0300

    create: function that prints before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+void start(void)__attribute__ ((constructor));
+
+/**
+ * start - prints before main
+ * Return: Nothing
+ */
+
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
