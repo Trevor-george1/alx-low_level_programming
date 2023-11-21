2bb2ccb38dea518961d954b6e18d59328a6d60ea
Trevor <Trevormahinda@gmai.com>
2023-11-21 19:57:03 +0300

    create: insert at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newnode = malloc(sizeof(listint_t));
 
-	if ((*head == NULL && idx != 0) || newnode == NULL)
+	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 
