3d3c13bdbd2f0b00c095ced68565c82f0dbb9806
Trevor <Trevormahinda@gmai.com>
2023-11-21 13:19:47 +0300

    update: delete head node and return data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,14 +8,17 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int data;
 
 	if (head == NULL)
 		return (0);
+	
+	data = (*head)->n;
 
 	temp = *head;
 
 	*head = (*head)->next;
 	free(temp);
 
-	return (temp->n);
+	return (data);
 }
