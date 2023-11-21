de91c8c6da25124000e8ad93a3168767199c1f43
Trevor <Trevormahinda@gmai.com>
2023-11-21 19:53:38 +0300

    create: insert at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: head
+ * @idx: index
+ * @n: data
+ *
+ * Return: newnode
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newnode, *temp;
+	unsigned int i;
+
+	newnode = malloc(sizeof(listint_t));
+
+	if ((*head == NULL && idx != 0) || newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	temp = *head;
+
+	for (i = 0; i < idx - 1 && head != NULL; i++)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+		{
+			return (NULL);
+		}
+	}
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+	}
+	else if (temp->next)
+	{
+		newnode->next = temp->next;
+		temp->next = newnode;
+	}
+	else
+	{
+		newnode->next = NULL;
+		temp->next = newnode;
+	}
+	return (newnode);
+}
