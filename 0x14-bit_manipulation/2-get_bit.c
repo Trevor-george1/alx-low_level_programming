545f54a5899a751efc16aa629cd44a5046d28d2f
Trevor <Trevormahinda@gmai.com>
2023-11-23 23:01:21 +0300

    create: value of bit at given position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - returns value of bit at a given position
+ * @n: number
+ * @index: index
+ *
+ * Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int m;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	m = 1UL << index;
+	return ((n & m) ? 1 : 0);
+}
