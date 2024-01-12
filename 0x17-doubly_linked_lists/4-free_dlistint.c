a29edd92b73476bfd678c97501ef252dea03b361
Trevor-george1 <Trevormahinda@gmail.com>
2024-01-12 11:26:18 -0800

    create: free a double list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dlistint - frees a dlistint
+ * @head: head
+ * Return: NOthing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+	}
+	free(head);
+	head = tmp;
+}
